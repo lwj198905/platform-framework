247ca6fcaaa5c5c777a258c75e87d7a3f3e84f2a
8025406 <rnf6053@dingtalk.com>
2019-07-26 14:57:54 +0800

    platform-framework

---
+++ platform-admin/src/main/java/com/platform/dao/SysOssDao.java
@@ -0,0 +1,13 @@
+package com.platform.dao;
+
+import com.platform.entity.SysOssEntity;
+
+/**
+ * 文件上传
+ *
+ * @author admin
+ * @date 2017年11月18日 下午13:13:23
+ */
+public interface SysOssDao extends BaseDao<SysOssEntity> {
+
+}
---
+++ platform-framework/src/test/java/com/platform/dao/TestSysUserDao.java
@@ -0,0 +1,14 @@
+package com.platform.dao;
+
+import com.platform.entity.SysUserEntity;
+
+/**
+ * 管理员测试Dao
+ *
+ * @author admin
+ * @email 714971203@qq.com
+ * @date 2018-07-09 10:21:36
+ */
+public interface TestSysUserDao extends BaseDao<SysUserEntity> {
+
+}
